5f830e46720895379b42c39805deef87f49e1790
Paul Rowe <paul.rowe@crownpeak.com>
2017-02-14 11:06:08 -0700

    Sets up component controller

--- src/app/switch/switch.component.ts
+++ src/app/switch/switch.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-switch',
   templateUrl: './switch.component.html',
   styleUrls: ['./switch.component.css']
 })
-export class SwitchComponent implements OnInit {
+export class SwitchComponent implements ControlValueAccessor {
+
+  @Input() label = 'switch';
+  @Input('value') _value = false;
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouched();
+  }
 
   constructor() { }
 
-  ngOnInit() {
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value) {
+    if (value) {
+      this.value = value;
+    }
+  }
+
+  switch() {
+    this.value = !this.value;
   }
 
 }
--- src/app/switch/switch.module.ts
+++ src/app/switch/switch.module.ts
@@ -1,12 +1,20 @@
 import { SwitchComponent } from './switch.component';
-import { NgModule } from '@angular/core';
+import { NgModule, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [SwitchComponent],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SwitchComponent),
+      multi: true
+    }
+  ],
   declarations: [SwitchComponent]
 })
 export class SwitchModule { }
